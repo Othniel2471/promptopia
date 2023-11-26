186dfffdbfdd647a2e3be182c74232d4dab38160
othniel2471 <othnielomaidu@gmail.com>
2023-11-26 12:36:54 +0100

    added mongodb and user sign in functionalities using google

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,19 +5,19 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
   const [providers, setProviders] = useState(null);
   const [toggleDropDown, settoggleDropDown] = useState(false);
 
   useEffect(() => {
-    const setProviders = async () => {
+    const setUpProviders = async () => {
       const response = await getProviders();
 
       setProviders(response);
     };
 
-    setProviders();
+    setUpProviders();
   }, []);
 
   return (
@@ -32,9 +32,10 @@ const Nav = () => {
         />
         <p className="logo_text">Promptopia</p>
       </Link>
+      {/* {alert(session?.user)} */}
       {/* desktop navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn capitalize">
               create post
@@ -50,7 +51,7 @@ const Nav = () => {
 
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -75,11 +76,11 @@ const Nav = () => {
       </div>
       {/* mobile navigation */}
       <div className="sm:hidden flex flex-col">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
-              alt="promptopia logo"
+              src={session?.user.image}
+              alt="profile"
               width={30}
               height={30}
               className="object-contain"
