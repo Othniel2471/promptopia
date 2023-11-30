ebbfd1c122bb12fb200b6935beec2b60106171cb
othniel2471 <othnielomaidu@gmail.com>
2023-11-30 22:48:32 +0100

    updated navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -52,6 +52,7 @@ const Nav = () => {
             <Link href="/profile">
               <Image
                 src={session?.user.image}
+                alt="profile"
                 width={37}
                 height={37}
                 className="rounded-full"
